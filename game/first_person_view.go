2a788407b14fb38a1005b248e1d018d502c91661
Darren <theinvader360@gmail.com>
2020-06-09 17:08:53 +0100

    First person view

--- game/first_person_view.go
+++ game/first_person_view.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
@@ -8,7 +9,8 @@ import (
 )
 
 var (
-	bgImg *ebiten.Image
+	bgImg    *ebiten.Image
+	wallImgs []*ebiten.Image
 )
 
 func init() {
@@ -16,11 +18,84 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	for i := 0; i < 12; i++ {
+		path := fmt.Sprintf("../assets/rgb/%d.png", i)
+		wallImg, _, err := ebitenutil.NewImageFromFile(path, ebiten.FilterDefault)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wallImgs = append(wallImgs, wallImg)
+	}
 }
 
 func renderFirstPersonView(g *Game, v *ebiten.Image) *ebiten.Image {
 	bgOp := &ebiten.DrawImageOptions{}
 	v.DrawImage(bgImg, bgOp)
+
+	//order: ffll, ffrr, ffl, ffr, ff, fll, frr, fl, fr, f, l, r
+	fovCells := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	switch g.player.dir {
+	case north:
+		fovCells[0] = getCell(g.player.x-2, g.player.y-2, g.gridMap)
+		fovCells[1] = getCell(g.player.x+2, g.player.y-2, g.gridMap)
+		fovCells[2] = getCell(g.player.x-1, g.player.y-2, g.gridMap)
+		fovCells[3] = getCell(g.player.x+1, g.player.y-2, g.gridMap)
+		fovCells[4] = getCell(g.player.x, g.player.y-2, g.gridMap)
+		fovCells[5] = getCell(g.player.x-2, g.player.y-1, g.gridMap)
+		fovCells[6] = getCell(g.player.x+2, g.player.y-1, g.gridMap)
+		fovCells[7] = getCell(g.player.x-1, g.player.y-1, g.gridMap)
+		fovCells[8] = getCell(g.player.x+1, g.player.y-1, g.gridMap)
+		fovCells[9] = getCell(g.player.x, g.player.y-1, g.gridMap)
+		fovCells[10] = getCell(g.player.x-1, g.player.y, g.gridMap)
+		fovCells[11] = getCell(g.player.x+1, g.player.y, g.gridMap)
+	case east:
+		fovCells[0] = getCell(g.player.x+2, g.player.y-2, g.gridMap)
+		fovCells[1] = getCell(g.player.x+2, g.player.y+2, g.gridMap)
+		fovCells[2] = getCell(g.player.x+2, g.player.y-1, g.gridMap)
+		fovCells[3] = getCell(g.player.x+2, g.player.y+1, g.gridMap)
+		fovCells[4] = getCell(g.player.x+2, g.player.y, g.gridMap)
+		fovCells[5] = getCell(g.player.x+1, g.player.y-2, g.gridMap)
+		fovCells[6] = getCell(g.player.x+1, g.player.y+2, g.gridMap)
+		fovCells[7] = getCell(g.player.x+1, g.player.y-1, g.gridMap)
+		fovCells[8] = getCell(g.player.x+1, g.player.y+1, g.gridMap)
+		fovCells[9] = getCell(g.player.x+1, g.player.y, g.gridMap)
+		fovCells[10] = getCell(g.player.x, g.player.y-1, g.gridMap)
+		fovCells[11] = getCell(g.player.x, g.player.y+1, g.gridMap)
+	case south:
+		fovCells[0] = getCell(g.player.x+2, g.player.y+2, g.gridMap)
+		fovCells[1] = getCell(g.player.x-2, g.player.y+2, g.gridMap)
+		fovCells[2] = getCell(g.player.x+1, g.player.y+2, g.gridMap)
+		fovCells[3] = getCell(g.player.x-1, g.player.y+2, g.gridMap)
+		fovCells[4] = getCell(g.player.x, g.player.y+2, g.gridMap)
+		fovCells[5] = getCell(g.player.x+2, g.player.y+1, g.gridMap)
+		fovCells[6] = getCell(g.player.x-2, g.player.y+1, g.gridMap)
+		fovCells[7] = getCell(g.player.x+1, g.player.y+1, g.gridMap)
+		fovCells[8] = getCell(g.player.x-1, g.player.y+1, g.gridMap)
+		fovCells[9] = getCell(g.player.x, g.player.y+1, g.gridMap)
+		fovCells[10] = getCell(g.player.x+1, g.player.y, g.gridMap)
+		fovCells[11] = getCell(g.player.x-1, g.player.y, g.gridMap)
+	case west:
+		fovCells[0] = getCell(g.player.x-2, g.player.y+2, g.gridMap)
+		fovCells[1] = getCell(g.player.x-2, g.player.y-2, g.gridMap)
+		fovCells[2] = getCell(g.player.x-2, g.player.y+1, g.gridMap)
+		fovCells[3] = getCell(g.player.x-2, g.player.y-1, g.gridMap)
+		fovCells[4] = getCell(g.player.x-2, g.player.y, g.gridMap)
+		fovCells[5] = getCell(g.player.x-1, g.player.y+2, g.gridMap)
+		fovCells[6] = getCell(g.player.x-1, g.player.y-2, g.gridMap)
+		fovCells[7] = getCell(g.player.x-1, g.player.y+1, g.gridMap)
+		fovCells[8] = getCell(g.player.x-1, g.player.y-1, g.gridMap)
+		fovCells[9] = getCell(g.player.x-1, g.player.y, g.gridMap)
+		fovCells[10] = getCell(g.player.x, g.player.y+1, g.gridMap)
+		fovCells[11] = getCell(g.player.x, g.player.y-1, g.gridMap)
+	}
+
+	for i := range fovCells {
+		if fovCells[i] == 1 {
+			wiOp := &ebiten.DrawImageOptions{}
+			v.DrawImage(wallImgs[i], wiOp)
+		}
+	}
 
 	return v
 }
