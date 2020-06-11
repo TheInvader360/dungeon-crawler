4724b688a2ed26098a71fbebc19c3767555955e6
Darren <theinvader360@gmail.com>
2020-06-11 18:10:59 +0100

    Add entity billboards to mockup

--- mockup/main.go
+++ mockup/main.go
@@ -19,14 +19,18 @@ const (
 )
 
 var (
-	avatar *ebiten.Image
-	bg     *ebiten.Image
-	cells  []cell
-	err    error
+	avatar     *ebiten.Image
+	bg         *ebiten.Image
+	entityFar  *ebiten.Image
+	entityMid  *ebiten.Image
+	entityNear *ebiten.Image
+	cells      []cell
+	err        error
 )
 
 type cell struct {
 	blocked bool
+	entity  bool
 	btnX    float64
 	btnY    float64
 	view    *ebiten.Image
@@ -41,10 +45,25 @@ func initGame() func(*ebiten.Image) error {
 	if err != nil {
 		log.Fatal(err)
 	}
+	entityFar, _, err = ebitenutil.NewImageFromFile("../assets/entityFar.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	entityMid, _, err = ebitenutil.NewImageFromFile("../assets/entityMid.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	entityNear, _, err = ebitenutil.NewImageFromFile("../assets/entityNear.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 	for i := 0; i < 12; i++ {
 		cell := cell{}
 		if i == 3 || i == 4 || i == 7 || i == 11 {
 			cell.blocked = true
+		}
+		if i < 5 || i > 6 {
+			cell.entity = true
 		}
 		if i < 5 {
 			cell.btnY = screenHeight - 3*cellSize
@@ -88,10 +107,13 @@ func initGame() func(*ebiten.Image) error {
 		cell.view = view
 		cells = append(cells, cell)
 	}
+
 	cellBlockedImage, _ := ebiten.NewImage(cellSize, cellSize, ebiten.FilterDefault)
 	cellBlockedImage.Fill(colornames.Sienna)
 	cellUnblockedImage, _ := ebiten.NewImage(cellSize, cellSize, ebiten.FilterDefault)
 	cellUnblockedImage.Fill(colornames.Antiquewhite)
+	cellEntityImage, _ := ebiten.NewImage(cellSize/2, cellSize/2, ebiten.FilterDefault)
+	cellEntityImage.Fill(colornames.Cornflowerblue)
 	return func(screen *ebiten.Image) error {
 		if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
 			x, y := ebiten.CursorPosition()
@@ -101,18 +123,70 @@ func initGame() func(*ebiten.Image) error {
 				}
 			}
 		}
+		if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonRight) {
+			x, y := ebiten.CursorPosition()
+			for i, cell := range cells {
+				if float64(x) > cell.btnX && float64(x) < cell.btnX+cellSize && float64(y) > cell.btnY && float64(y) < cell.btnY+cellSize {
+					cells[i].entity = !cells[i].entity
+				}
+			}
+		}
 		if ebiten.IsDrawingSkipped() {
 			return nil
 		}
 		screen.Fill(colornames.Grey)
 		bgOp := &ebiten.DrawImageOptions{}
-		//bgOp.GeoM.Scale(0.5, 0.5)
 		screen.DrawImage(bg, bgOp)
-		for _, cell := range cells {
-			viewOp := &ebiten.DrawImageOptions{}
-			//viewOp.GeoM.Scale(0.5, 0.5)
+		for i, cell := range cells {
+			if cell.entity {
+				entityOp := &ebiten.DrawImageOptions{}
+				//FAR, L-R
+				if i == 0 {
+					entityOp.GeoM.Translate(-10, 20)
+					screen.DrawImage(entityFar, entityOp)
+				}
+				if i == 2 {
+					entityOp.GeoM.Translate(10, 20)
+					screen.DrawImage(entityFar, entityOp)
+				}
+				if i == 4 {
+					entityOp.GeoM.Translate(30, 20)
+					screen.DrawImage(entityFar, entityOp)
+				}
+				if i == 3 {
+					entityOp.GeoM.Translate(50, 20)
+					screen.DrawImage(entityFar, entityOp)
+				}
+				if i == 1 {
+					entityOp.GeoM.Translate(70, 20)
+					screen.DrawImage(entityFar, entityOp)
+				}
+				//MID, L-R
+				if i == 7 {
+					entityOp.GeoM.Translate(-5, 15)
+					screen.DrawImage(entityMid, entityOp)
+				}
+				if i == 9 {
+					entityOp.GeoM.Translate(25, 15)
+					screen.DrawImage(entityMid, entityOp)
+				}
+				if i == 8 {
+					entityOp.GeoM.Translate(55, 15)
+					screen.DrawImage(entityMid, entityOp)
+				}
+				//NEAR, L-R
+				if i == 10 {
+					entityOp.GeoM.Translate(-41, 3)
+					screen.DrawImage(entityNear, entityOp)
+				}
+				if i == 11 {
+					entityOp.GeoM.Translate(67, 3)
+					screen.DrawImage(entityNear, entityOp)
+				}
+			}
 			if cell.blocked {
-				screen.DrawImage(cell.view, viewOp)
+				wallOp := &ebiten.DrawImageOptions{}
+				screen.DrawImage(cell.view, wallOp)
 			}
 			btnOp := &ebiten.DrawImageOptions{}
 			btnOp.GeoM.Translate(cell.btnX, cell.btnY)
@@ -120,6 +194,10 @@ func initGame() func(*ebiten.Image) error {
 				screen.DrawImage(cellBlockedImage, btnOp)
 			} else {
 				screen.DrawImage(cellUnblockedImage, btnOp)
+			}
+			if cell.entity {
+				btnOp.GeoM.Translate(cellSize/4, cellSize/4)
+				screen.DrawImage(cellEntityImage, btnOp)
 			}
 		}
 		avatarOp := &ebiten.DrawImageOptions{}
